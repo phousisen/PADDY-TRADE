2658e2bc5acf4ce1c3eeb0ff08135f2bf1966fde
phousisen <92678341+phousisen@users.noreply.github.com>
2026-09-22 18:39:03 +0700

    Delete UNINSTALL_WATCHDOG.bat

--- UNINSTALL_WATCHDOG.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Removes the watchdog. Run as administrator. The scale program is not touched.
-schtasks /end /tn "PaddyTrade Scale Watchdog" >nul 2>&1
-schtasks /delete /tn "PaddyTrade Scale Watchdog" /f
-echo Watchdog removed.
-pause
